7bc7983f0cee5fe3fb339195b10c8d5a682d788b
Alexey Lebedev <lebedev.alexey@protonmail.com>
2019-05-16 19:50:42 +1000

    add notes output, add templates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask
+import sqlite3
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -12,4 +13,13 @@ DATABASE = 'database.db'
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    connect = sqlite3.connect(DATABASE)
+    cursor = connect.cursor()
+    notes_query = cursor.execute("SELECT * FROM notes")
+    headers = tuple(map(lambda description: description[0], notes_query.description))
+    notes = notes_query.fetchall()
+    notes_dicts = list(
+        map(lambda row: dict(zip(headers, row)), notes)
+    )
+
+    return render_template('index.html', app_name=APP_NAME, notes=notes_dicts)
